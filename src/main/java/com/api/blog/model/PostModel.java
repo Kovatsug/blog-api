6fcdd476715d5fdbaa4e4be19e5c4159520814ed
GUSTAVO HENRIQUE KREUCH <gustavo_h_kreuch@estudante.sesisenai.org.br>
2026-09-16 21:25:36 -0300

    aplication properties & add PostModel

---
+++ src/main/java/com/api/blog/model/PostModel.java
@@ -0,0 +1,84 @@
+package com.api.blog.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_POST")
+public class PostModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 70)
+    private String autor;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false, length = 100)
+    private String titulo;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    private String texto;
+
+    public PostModel() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public PostModel(UUID id, String autor, LocalDate data, String titulo, String texto) {
+        this.id = id;
+        this.autor = autor;
+        this.data = data;
+        this.titulo = titulo;
+        this.texto = texto;
+    }
+}
