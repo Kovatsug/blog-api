5bcdc57a4b9d5800cd9cc32818737b9601ccf8dd
GUSTAVO HENRIQUE KREUCH <gustavo_h_kreuch@estudante.sesisenai.org.br>
2026-09-16 21:40:39 -0300

    add PostReposytory

---
+++ src/main/java/com/api/blog/Repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.api.blog.Repository;
+
+import com.api.blog.model.PostModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<PostModel, UUID> {
+
+
+
+}
