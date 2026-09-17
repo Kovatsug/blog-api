d420dd405fb3c4562e4092170bb3c94a98f6f6c7
GUSTAVO HENRIQUE KREUCH <gustavo_h_kreuch@estudante.sesisenai.org.br>
2026-09-17 19:17:22 -0300

    refactor to record & desenvolvido

--- src/main/java/com/api/blog/dto/request/PostRequestDto.java
+++ src/main/java/com/api/blog/dto/request/PostRequestDto.java
@@ -1,4 +1,18 @@
 package com.api.blog.dto.request;
 
-public class PostRequestDto {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostRequestDto(
+        @NotBlank(message = "O autor é obrigatório")
+        @Size(max = 70,message = "Autor deve ter um máximo de 70 caracteres")
+        String autor,
+
+        @NotBlank(message = "Titulo obrigatorio")
+        @Size(max = 100, message = "Titulo maximo de 100 caracteres")
+        String titulo,
+
+        @NotBlank(message = "O texto é obrigatório")
+        String texto
+) {
 }
--- src/main/java/com/api/blog/dto/response/PostResponseDto.java
+++ src/main/java/com/api/blog/dto/response/PostResponseDto.java
@@ -1,4 +1,15 @@
 package com.api.blog.dto.response;
 
-public class PostResponseDto {
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record PostResponseDto(
+        UUID id,
+        String autor,
+        LocalDate data,
+        String titulo,
+        String texto
+) {
 }
